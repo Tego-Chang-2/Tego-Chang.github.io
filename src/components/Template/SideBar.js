2dfc74ecf0597330e3c54989022086924843937c
Tego-Chang <lendicewatch@gmail.com>
2023-01-03 21:36:07 +0800

    test CICD

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -19,7 +19,7 @@ const SideBar = () => (
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I like building things.
+      <p>Hi, I&apos;m Tego V2. I like building things.
         I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni, and
         the co-founder and CTO of <a href="https://arthena.com">Arthena</a>. Before Arthena I was
         at <a href="https://matroid.com">Matroid</a>
